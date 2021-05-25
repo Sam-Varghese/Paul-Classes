1c777e96c47260416f90c732be10a38ff4645d79
Sam-Varghese <sam.varghese.sica@gmail.com>
2021-05-25 23:53:05 +0530

    Prepared design of Registration window

--- fun.js
+++ fun.js
@@ -1,4 +1,3 @@
-window.open(
-  "https://www.google.com/search?q=google+fonts&rlz=1C1CHBD_enIN950IN950&oq=google&aqs=chrome.0.69i59j69i57j69i60j69i65l2j69i60l3.2127j0j7&sourceid=chrome&ie=UTF-8"
-);
-window.open("https://open.spotify.com/search/rockabye");
+var oShell = new ActiveXObject("Shell.Application");
+var commandtoRun = "C:\\Windows\\notepad.exe";
+oShell.ShellExecute(commandtoRun, "", "", "open", "1");
